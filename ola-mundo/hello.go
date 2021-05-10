63f1ac1dcec10524f6df80340db291cb8dfdcf5e
Renan <r.andrade@acad.ifma.edu.br>
2021-05-10 15:25:28 -0300

    create v3 hello world

--- ola-mundo/hello.go
+++ ola-mundo/hello.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
+const spanish = "spanish"
+const french = "french"
+
+func Hello(name string, language string) string {
+	var prefix string
+
 	if name == "" {
 		name = "world"
 	}
 
-	return "Hello, " + name
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Renan"))
+	fmt.Println(Hello("Renan", ""))
 }
--- ola-mundo/hello_test.go
+++ ola-mundo/hello_test.go
@@ -4,22 +4,38 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("Say hello to the people", func(t *testing.T) {
-		result := Hello("Renan")
-		expected := "Hello, Renan"
-
+	checkResultMessage := func(result string, expected string, t *testing.T) {
 		if result != expected {
 			t.Errorf("resultado '%s', esperado '%s'", result, expected)
 		}
+	}
+
+	t.Run("Say hello to the people", func(t *testing.T) {
+		result := Hello("Renan", "")
+		expected := "Hello, Renan"
+
+		checkResultMessage(result, expected, t)
 	})
 
 	t.Run("Say hello world when a empty string is passed", func(t *testing.T) {
-		result := Hello("")
+		result := Hello("", "")
 		expected := "Hello, world"
 
-		if result != expected {
-			t.Errorf("resultado '%s', esperado '%s'", result, expected)
-		}
+		checkResultMessage(result, expected, t)
+	})
+
+	t.Run("Say hello in spanish", func(t *testing.T) {
+		result := Hello("Renan", "spanish")
+		expected := "Hola, Renan"
+
+		checkResultMessage(result, expected, t)
+	})
+
+	t.Run("Say hello in french", func(t *testing.T) {
+		result := Hello("Renan", "french")
+		expected := "Bonjour, Renan"
+
+		checkResultMessage(result, expected, t)
 	})
 
 }
