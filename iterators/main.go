f01fefe787841e2edd92a43a41b8b66cf2e86414
Renan <r.andrade@acad.ifma.edu.br>
2021-05-10 17:33:17 -0300

    create v3 iterators

--- iterators/main.go
+++ iterators/main.go
@@ -1,7 +1,14 @@
 package iterators
 
+const repeatQtd = 4
+
 func Repeat(iterator string) string {
-	return "aaaa"
+	var repeat string
+	for i := 0; i < repeatQtd; i++ {
+		repeat += iterator
+	}
+
+	return repeat
 }
 
 func main() {
